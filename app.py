19d2382de0a9800731abdcae987eafa316445ee4
Olamidun <kolapoolamidun@gmail.com>
2022-06-27 03:10:32 +0100

    add a mini endpoint that returns a json response of hello as key and world as value

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from src import create_app
+# from flask_restful import Resource, Api
 
-app = create_app()
+# app = create_app()
+# api = Api(app)
 
-app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+# api.add_resource(HelloWorld, '/hello_world')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
+from flask_restful import Api
+from .endpoints import HelloWorld
 from flask_sqlalchemy import SQLAlchemy
-
 
 db = SQLAlchemy()
 def create_app():
@@ -10,7 +11,10 @@ def create_app():
     # Get the corresponding database uri from docker-compose
     app_settings = os.environ.get('APP_SETTINGS')
     app.config.from_object(app_settings)
-
     # set up extensions
     db.init_app(app)
+    
+    api = Api(app)
+
+    api.add_resource(HelloWorld, '/hello_world')
     return app
